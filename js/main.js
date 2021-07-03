eabfb5717af79ddd93920aed164ae45089ccf069
fabiorisantos <fabiorisantos1981@gmail.com>
2021-07-03 20:49:05 -0300

    introducao_java_script
    Introducao ao JavaScript

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var nome = "Fábio Ribeiro dos Santos";
+var idade = 39;
+var idade2 = 10;
+var frase = "Brasil é o melhor país do mundo"
+//alert(nome + " tem " + idade + " anos");
+//alert (idade + idade2);
+console.log(nome);
+console.log(idade+idade2);
+console.log(frase.toLowerCase());
+//alert(nome.replace("Fábio","Christian Javier Quispe"));
