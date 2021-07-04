b70e3c3b557b323efd41884dd8678c0746562081
fabiorisantos <fabiorisantos1981@gmail.com>
2021-07-03 21:13:28 -0300

    array_dicionario
    Array e dicionário

--- js/main.js
+++ js/main.js
@@ -1,10 +1,33 @@
-var nome = "Fábio Ribeiro dos Santos";
-var idade = 39;
-var idade2 = 10;
-var frase = "Brasil é o melhor país do mundo"
+var frutas = [{nome:"maça", cor:"vermelha"} , {nome:"uva", cor:"roxa"} , {nome:"laranja", cor:"amarela"}];
+console.log(frutas);
+console.log(frutas[0].nome);
+alert (frutas[1].cor);
+
+
+/*
+var fruta = {nome:"maça", cor:"vermelha"};
+console.log(fruta);
+console.log(fruta.nome);
+alert (fruta.cor);
+*/
+
+//var lista = ["maça" , "perâ", "laranja"];
+//lista.push("uva");
+//lista.pop();
+//console.log(lista);
+//console.log(lista.length);
+//console.log(lista.reverse());
+//console.log(lista.toString());
+//console.log(lista.join(" | "));
+
+
+//var nome = "Fábio Ribeiro dos Santos";
+//var idade = 39;
+//var idade2 = 10;
+//var frase = "Brasil é o melhor país do mundo"
 //alert(nome + " tem " + idade + " anos");
 //alert (idade + idade2);
-console.log(nome);
-console.log(idade+idade2);
-console.log(frase.toLowerCase());
+//console.log(nome);
+//console.log(idade+idade2);
+//console.log(frase.toLowerCase());
 //alert(nome.replace("Fábio","Christian Javier Quispe"));
